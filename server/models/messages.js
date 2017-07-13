bc4a0b50d8a0a365280fa8606d147dc88e046283
ludralph <raphaelumoh@gmail.com>
2017-07-13 04:47:35 +0100

    Updated Message model to use export default instead of module.exports and switched to Sequelize.datatype to fix a bug with Datatypes.datatype in version 4.3 of sequelize

--- server/models/messages.js
+++ server/models/messages.js
@@ -1,54 +1,62 @@
 /* eslint linebreak-style: ['error', 'windows']*/
-module.exports = (sequelize, DataType) => {
-  const Messages = sequelize.define('Messages', {
-    id: {
-      type: DataType.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull: false,
-    },
-    message: {
-      type: DataType.Text,
-      allowNull: false,
-      validate: {
-        notEmpty: true
-      }
-    },
-    priority: {
-      type: DataType.ENUM,
-      values: ['normal', 'urgent', 'critical']
-    },
-    userId: {
-      type: DataType.INTEGER,
-      onDelete: 'CASCADE',
-      references: {
-        model: 'Users',
-        key: 'id',
-        as: 'userId'
-      }
-    },
-    groupId: {
-      type: DataType.INTEGER,
-      onDelete: 'CASCADE',
-      references: {
-        model: 'Groups',
-        key: 'id',
-        as: 'groupId'
-      }
-    },
-    classMethods: {
-      associate: (models) => {
-        // associations can be defined here
-        Messages.belongsTo(models.Users, {
-          foreignKey: 'userId',
-          onDelete: 'CASCADE',
-        });
-        Messages.belongsTo(models.Groups, {
-          foreignKey: 'groupId',
-          onDelete: 'CASCADE',
-        });
-      }
+
+import Sequelize from 'sequelize';
+
+
+const sequelize = new Sequelize('mockpostit', 'postgres', 'bootcamp', {
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+const Groups = sequelize.define('GroupMembers', {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER
+  },
+  admin: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+  userId: {
+    type: Sequelize.INTEGER,
+    onDelete: 'CASCADE',
+    references: {
+      model: 'Users',
+      key: 'id',
+      as: 'userId'
+    }
+  },
+  groupId: {
+    type: Sequelize.INTEGER,
+    onDelete: 'CASCADE',
+    references: {
+      model: 'Groups',
+      key: 'id',
+      as: 'groupId'
     }
   }
-  );
-};
+}, {
+  classMethods: {
+    associate: (models) => {
+      // associations can be defined here
+      Groups.belongsTo(models.Users, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE'
+      });
+      Groups.belongsTo(models.Groups, {
+        foreignKey: 'groupId',
+        onDelete: 'CASCADE'
+      });
+    }
+  }
+});
+
+export default Groups;
+
