a7e7a88f0db210f748d72509924b9aa741bbaf13
ludralph <raphaelumoh@gmail.com>
2017-07-13 07:02:09 +0100

    webpack configuration file for converting sass to css added to the repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,48 @@
-const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+/* eslint linebreak-style: ['error', 'windows']*/
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
-    filename: "[name].[contenthash].css",
-    disable: process.env.NODE_ENV === "development"
+  filename: '[name].[contenthash].css',
+  disable: process.env.NODE_ENV === 'development'
 });
 module.exports = {
-    
-    //devtool: debug ? "inline-sourcemap" : false,
-    entry: "./src/main.js",
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015', 'stage-0']
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: extractSass.extract({
-                    use: [{
-                        loader: "css-loader"
-                    }, {
-                        loader: "sass-loader"
-                    }],
-                    // use style-loader in development
-                    fallback: "style-loader"
 
-                })
-            }
-        
-        ]
-    },
-    output: {
-        path: __dirname + "dist",
-        filename: "bundle.min.js"
-    },
-    plugins: [
-        extractSass
+  // devtool: debug ? "inline-sourcemap" : false,
+  entry: 'index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [{
+            loader: 'css-loader'
+          }, {
+            loader: 'sass-loader'
+          }],
+          // use style-loader in development
+          fallback: 'style-loader'
+
+        })
+      }
+
     ]
-    // plugins: debug ? [] : [
-    //     new webpack.optimize.DedupePlugin(),
-    //     new webpack.optimize.OccurrenceOrderPlugin(),
-    //     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-    // ],
+  },
+  output: {
+    path: `${__dirname}dist`,
+    filename: 'bundle.min.js'
+  },
+  node: {
+    fs: 'empty'
+  },
+  plugins: [
+    extractSass
+  ]
 };
