f3196f7b26b0c0fdd1d35a1213ecf2def89728a9
ludralph <raphaelumoh@gmail.com>
2017-07-13 04:44:21 +0100

    Updated Group model to use export default instead of module.exports and switched to Sequelize.datatype to fix a bug with Datatypes.datatype in version 4.3 of sequelize

--- server/models/group.js
+++ server/models/group.js
@@ -1,43 +1,56 @@
 /* eslint linebreak-style: ['error', 'windows']*/
 
-module.exports = (sequelize, DataType) => {
-  const Groups = sequelize.define('Groups', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataType.INTEGER
-    },
-    groupName: {
-      type: DataType.STRING,
-      allowNull: false,
-      unique: true
-    },
-    description: {
-      type: DataType.STRING
-    },
-    userId: {
-      type: DataType.INTEGER,
-      onDelete: 'CASCADE',
-      references: {
-        model: 'Users',
-        key: 'id',
-        as: 'userId'
-      }
-    },
-    classMethods: {
-      associate: (models) => {
-        // associations can be defined here
-        Groups.hasMany(models.GroupUsers, {
-          foreignKey: 'groupId',
-          as: 'groupId'
-        });
-        Groups.belongsTo(models.Users, {
-          foreignKey: 'userId',
-          onDelete: 'CASCADE',
-        });
-      }
+import Sequelize from 'sequelize';
+
+
+const sequelize = new Sequelize('mockpostit', 'postgres', 'bootcamp', {
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+const Group = sequelize.define('Groups', {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER
+  },
+  groupName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  description: {
+    type: Sequelize.STRING
+  },
+  userId: {
+    type: Sequelize.INTEGER,
+    onDelete: 'CASCADE',
+    references: {
+      model: 'Users',
+      key: 'id',
+      as: 'userId'
     }
-  });
-  return Groups;
-};
+  }
+}, {
+  classMethods: {
+    associate: (models) => {
+        // associations can be defined here
+      Group.hasMany(models.GroupMembers, {
+        foreignKey: 'groupId',
+        as: 'groupId'
+      });
+      Group.belongsTo(models.Users, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE',
+      });
+    }
+  }
+});
+
+export default Group;
+
