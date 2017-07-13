ddc3fedd737823db7a4e1c017a2f70e4c97bed37
ludralph <raphaelumoh@gmail.com>
2017-07-13 06:52:20 +0100

    Routes file for the API endpoints are defined here along with reference to the controller and validator

---
+++ server/routes/index.js
@@ -0,0 +1,29 @@
+/* eslint linebreak-style: ['error', 'windows']*/
+import express from 'express';
+import controller from '../controllers/controller';
+import Validate from '../validator';
+
+const router = express.Router();
+
+// Route for user signup
+router.post('/signup', Validate.signupInputs, controller.signup);
+
+// Route for signin
+router.post('/signin', Validate.signinInputs, controller.signin);
+
+// Middleware to protect routes
+router.use(controller.ensureToken);
+
+// Route to post group create info
+router.post('/group', Validate.createGroupInputs, controller.createGroup);
+
+// Route to add users to group
+router.post('/group/:groupId/user', Validate.groupsInputs, controller.groups);
+
+// Route to post messages to groups
+router.post('/group/:groupId/messages', Validate.messagesInputs, controller.messages);
+
+// Route to get messages posted to groups
+router.get('/group/:groupId/messages', controller.getMessages);
+
+export default router;
