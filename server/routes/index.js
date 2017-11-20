21096df1f2dfac1194fafb266dd60e046567fc9d
Raphael Etim <andeladeveloper@Andelas-MacBook-Pro-4.local>
2017-11-20 12:22:40 +0100

    c reating api routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,28 +1,47 @@
 import express from 'express';
-import controller from '../controllers/controller';
-import Validate from '../validator';
+import authenticate from '../middlewares/authenticate';
+import validateInput from '../middlewares/validateInput';
+import user from '../controllers/user';
+import group from '../controllers/group';
+import message from '../controllers/message';
 
 const router = express.Router();
 
-// Route for user signup
-router.post('/signup', Validate.signupInputs, controller.signup);
+// route for user sign up
+router.post('/api/signup', validateInput.validateSignupInput, user.signup);
 
-// Route for signin
-router.post('/signin', Validate.signinInputs, controller.signin);
+// route for user sign in
+router.post('/api/signin', user.signin);
 
-// Middleware to protect routes
-router.use(controller.ensureToken);
+// route to update password
+router.put('/api/forgotpassword', user.forgotPassword);
 
-// Route to post group create info
-router.post('/group', Validate.createGroupInputs, controller.createGroup);
+// route to reset password
+router.put('/api/resetpassword/:token', user.resetPassword);
 
-// Route to add users to group
-router.post('/group/:groupId/user', Validate.groupsInputs, controller.addUser);
+// route to search for users
+router.get('/api/search/users', authenticate.verifyUser, user.searchUser);
 
-// Route to post messages to groups
-router.post('/group/:groupId/messages', Validate.messagesInputs, controller.messages);
+router.get('/api/user/:userId/groups', authenticate.verifyUser, user.listGroups);
 
-// Route to get messages posted to groups
-router.get('/group/:groupId/messages', controller.getMessages);
+// route to create groups
+router.post('/api/group', authenticate.verifyUser, validateInput.validateGroupname, group.create);
+
+// route to update a group
+router.put('/api/group/:groupId', authenticate.verifyUser, validateInput.validateGroupname, group.edit);
+
+// route to get a group by it
+router.get('/api/group/:groupId', authenticate.verifyUser, group.get);
+
+// route to add user to a group
+router.post('/api/group/:groupId/user', authenticate.verifyUser, group.addUser);
+// route to list users in a group
+router.get('/api/group/:groupId/users', authenticate.verifyUser, group.listUsers);
+
+// route to post message to a group
+router.post('/api/group/:groupId/message', authenticate.verifyUser, validateInput.validateMessageInput, message.create);
+
+// route to retrieve messages from a particular group
+router.get('/api/group/:groupId/messages', authenticate.verifyUser, message.list);
 
 export default router;
