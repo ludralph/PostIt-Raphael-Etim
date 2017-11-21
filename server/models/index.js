211f35bfd58b3b37c1262329fff3c0d955f7e869
Raphael Etim <andeladeveloper@Andelas-MacBook-Pro-4.local>
2017-11-21 07:50:11 +0100

    Read through database models and make them available app wide

---
+++ server/models/index.js
@@ -0,0 +1,45 @@
+// Import files for database models
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+import * as DbConfig from '../config/config.json';
+
+dotenv.config();
+
+const basename = path.basename(module.filename);
+const env = process.env.NODE_ENV || 'development';
+const config = DbConfig[env];
+
+const db = {};
+// Configure Sequelize
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], { logging: false });
+} else {
+  sequelize = new Sequelize(config.database, config.username,
+    config.password, {
+      dialect: 'postgres',
+    });
+}
+fs
+.readdirSync(__dirname)
+.filter(file =>
+  (file.indexOf('.') !== 0) &&
+  (file !== basename) &&
+  (file.slice(-3) === '.js'))
+.forEach((file) => {
+  const model = sequelize.import(path.join(__dirname, file));
+  db[model.name] = model;
+});
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
