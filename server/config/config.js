eded99beb92d02066ca64f31c8b52e4428fb5793
Raphael Etim <andeladeveloper@Andelas-MacBook-Pro-4.local>
2017-11-27 12:34:09 +0100

    deleted obsolete test case and updated database configurations

---
+++ server/config/config.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const config = {
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres'
+  },
+  test: {
+    url: process.env.TEST_DATABASE_URL,
+    dialect: 'postgres',
+    logging: false
+  },
+  production: {
+    url: process.env.PRODUCTION_DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true
+    }
+  }
+};
+
+module.exports = config;
