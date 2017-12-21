a780f255bb675d39253acfbd60cef19af8558127
ludralph <raphaelumoh@gmail.com>
2017-12-21 09:52:09 +0100

    end to end test

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
 
-models.sequelize.sync().then(() => {
+models.sequelize.sync({force: true }).then(() => {
   app.listen(port, () => {
     console.log(`Listening at port ${port}`);
   });
