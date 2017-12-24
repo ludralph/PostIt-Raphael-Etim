6918fe5dad8e21496de6a7cfc70a130ad59c3d3e
ludralph <raphaelumoh@gmail.com>
2017-12-24 10:13:52 +0100

    Revert "Taskrunner feedback implementation"

--- webpack.deployment.config.js
+++ webpack.deployment.config.js
@@ -59,9 +59,15 @@ module.exports = {
     extensions: ['.js', '.jsx']
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
     }),
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
   ],
 };
